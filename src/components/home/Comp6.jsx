9e9adb303cd2b7711ec5b5d339594ef7f9bfb15f
SandeepSangvekar <sandip.sangvekar@gmail.com>
2025-03-20 14:27:11 +0530

    custo partner of home and service changes

--- src/components/home/Comp6.jsx
+++ src/components/home/Comp6.jsx
@@ -1,3 +1,4 @@
+import Marquee from "react-fast-marquee";
 export const Comp6 = () => {
     return (
         <>
@@ -5,7 +6,7 @@ export const Comp6 = () => {
                 <div className="custom-container">
                     <h2 className="mb-4">Our customers & Partner</h2>
                 </div>
-                <div className="customer-partner">
+                {/* <div className="customer-partner">
                     <div>
                         <img src="/images/DSC-img.png" className="img-fluid" alt="" />
                     </div>
@@ -25,6 +26,25 @@ export const Comp6 = () => {
                     <div>
                         <img src="/images/gepdec-img.png" className="img-fluid" alt="" />
                     </div>
+                </div> */}
+                <div className="customer-partner">
+                    <Marquee speed={50} pauseOnHover={true} gradient={false}>
+                        {/* Duplicate the images to ensure seamless looping */}
+                        <img src="/images/DSC-img.png" className="img-fluid" alt="" />
+                        <img src="/images/ajax-img.png" className="img-fluid" alt="" />
+                        <img src="/images/ovino-img.png" className="img-fluid" alt="" />
+                        <img src="/images/ace-img.png" className="img-fluid" alt="" />
+                        <img src="/images/apollo-img.png" className="img-fluid" alt="" />
+                        <img src="/images/gepdec-img.png" className="img-fluid" alt="" />
+
+                        {/* Duplicate again to create a seamless transition */}
+                        <img src="/images/DSC-img.png" className="img-fluid" alt="" />
+                        <img src="/images/ajax-img.png" className="img-fluid" alt="" />
+                        <img src="/images/ovino-img.png" className="img-fluid" alt="" />
+                        <img src="/images/ace-img.png" className="img-fluid" alt="" />
+                        <img src="/images/apollo-img.png" className="img-fluid" alt="" />
+                        <img src="/images/gepdec-img.png" className="img-fluid" alt="" />
+                    </Marquee>
                 </div>
             </div>
         </>
