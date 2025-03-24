07c10b2df14be3db658cc0106b490e1f7fec8cff
SandeepSangvekar <sandip.sangvekar@gmail.com>
2025-03-24 12:25:17 +0530

    suc stories and career banner changes

--- src/components/career/Comp1.jsx
+++ src/components/career/Comp1.jsx
@@ -1,8 +1,8 @@
 export const Comp1 = () => {
     return (
         <>
-            <div className="career-banner">
-                <img src="/images/career-banner1.webp" className="img-fluid" alt="" />
+            <div className="career-banner text-center">
+                <img src="/images/career-bn1.webp" className="img-fluid" alt="" />
             </div>
         </>
     )
